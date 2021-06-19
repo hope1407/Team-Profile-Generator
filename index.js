b28abc473a2ba0d44e0b8010269bdcca27d62f01
Hope <johnhope354@gmail.com>
2021-06-19 15:16:44 -0500

    added dependencies, installed dependencies, got prompts working.

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generatePage = require('./src/generatePage')
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'list',
+        name: 'title',
+        message: 'What is your job title?',
+        choices: ['Manager','Engineer','Intern']
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Which license would you like to use?',
+        choices: ["MIT", "BSD3"]
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your application using two to three sentences.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Who can use this application and how?'
+      },
+    ]);
+  };
+  
+  // const generateREADME = (answers) =>
+  // `## Welcome to ${answers.name}!`;
+  
+  const init = () => {
+    promptUser()
+      .then((answers) => {
+        console.log('Successfully wrote a README file');
+        writeFileAsync('./dist/index.html', generatePage(answers));
+      })
+      .catch((err) => console.error(err));
+  };
+  
+  init();
